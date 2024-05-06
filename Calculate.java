5aedc4fd4134b14b35dba6bb507200f728e26235
Ammbareen <shehlaammy1108@gmail.com>
2024-05-06 16:55:41 +0530

    Added sum method to main branch

--- Calculate.java
+++ Calculate.java
@@ -1,11 +1,14 @@
 public class Calculate {
     public static void main(String[] args){
         int a=10,b=20;
+
         System.out.println("Print sum");
         System.out.println(sum(a,b));
     }
     public static int sum(int a, int b){
 
         return a+b+10;
+
     }
+
 }
