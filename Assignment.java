53ff96a1ef1c3421fa15373516c1bd12b97a8552
Ammbareen <shehlaammy1108@gmail.com>
2024-05-07 11:25:45 +0530

    Updated Assignment class with different print() message and added add() method

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+public class Assignment {
+    public static void main(String[] args){
+        int a=10,b=20;
+        System.out.println(print());
+        System.out.println(add(a,b));
+    }
+    public static String print(){
+        return "Hello Git";
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
