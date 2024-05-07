4053c035d8a34e950571f6920fd2be3264c98743
Ammbareen <shehlaammy1108@gmail.com>
2024-05-07 11:25:45 +0530

    Added Assignment class with print method to Test

---
+++ Assignment.java
@@ -0,0 +1,8 @@
+public class Assignment {
+    public static void main(String[] args){
+        System.out.println(print());
+    }
+    public static String print(){
+        return "Hello World";
+    }
+}
