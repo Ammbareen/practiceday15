57094e945d56290ff7a0502bc77b934b27fb2ac0
Ammbareen <shehlaammy1108@gmail.com>
2024-05-07 13:19:39 +0530

    Resolved conflicts in Temporary.java

---
+++ Temporary.java
@@ -0,0 +1,9 @@
+public class Temporary {
+    public static void main(String[] args){
+        System.out.println(tempprint());
+    }
+    public static String tempprint(){
+
+        return "Welcome to Temp";
+    }
+}
